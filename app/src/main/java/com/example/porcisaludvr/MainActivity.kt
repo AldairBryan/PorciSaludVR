d18b41e1b53bfb1cda1350aa6335335e9e6c585c
AldairBryan <asalcedoch@unsa.edu.pe>
2023-10-11 23:05:41 -0500

    Botones arreglados

--- app/src/main/java/com/example/porcisaludvr/MainActivity.kt
+++ app/src/main/java/com/example/porcisaludvr/MainActivity.kt
@@ -3,22 +3,38 @@ package com.example.porcisaludvr
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -47,7 +63,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(navController: NavHostController) {
-    val buttons = listOf(
+    val listState = rememberLazyListState()
+    val buttonItems = listOf(
         ButtonItem("Realidad Aumentada", R.drawable.ic_launcher_foreground, "ruta_boton1"),
         ButtonItem("Enfermedades", R.drawable.ic_launcher_foreground, "ruta_boton2"),
         ButtonItem("Sobre el Cuidado", R.drawable.ic_launcher_foreground, "ruta_boton3"),
@@ -56,30 +73,43 @@ fun MainScreen(navController: NavHostController) {
         ButtonItem("Sobre Nosotros", R.drawable.ic_launcher_foreground, "info_screen")
     )
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceEvenly,
-        horizontalAlignment = Alignment.CenterHorizontally
+    LazyColumn(
+        state = listState,
+        modifier = Modifier.fillMaxSize()
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-
-        }
-        buttons.forEach { button ->
-            Button(
-                onClick = {
-                    navController.navigate(button.route)
-                },
-                modifier = Modifier.fillMaxWidth()
+        itemsIndexed(buttonItems.chunked(2)) { rowIndex, rowItems ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Icon(imageVector = ImageVector.vectorResource(id = button.imageResource), contentDescription = null)
-                    Text(text = button.label, fontFamily = Itim)
+                for ((index, item) in rowItems.withIndex()) {
+                    Button(
+                        onClick = {
+                            // Navega a la ruta cuando se hace clic en el botón
+                            navController.navigate(item.route)
+                        },
+                        modifier = Modifier
+                            .width(GetScreenWidth() / 2)
+                            .height(GetScreenHeight()/3)
+                            .background(Color.White)
+                            .padding(8.dp)
+                    ) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Image(
+                                painter = painterResource(id = item.imageResource),
+                                contentDescription = null, // Puedes proporcionar una descripción adecuada aquí
+                                modifier = Modifier
+                                    .size(64.dp)
+                                    .padding(4.dp)
+                            )
+                            Text(text = item.label,
+                                fontFamily = Itim,
+                                fontSize = )
+                        }
+                    }
                 }
             }
         }
@@ -87,3 +117,31 @@ fun MainScreen(navController: NavHostController) {
 }
 
 data class ButtonItem(val label: String, val imageResource: Int, val route: String)
+
+@Preview
+@Composable
+fun PreviewMain(){
+    val navController = rememberNavController()
+    MainScreen(navController)
+}
+
+@Composable
+fun GetScreenWidth(): Dp {
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp
+
+    // Si deseas obtener el ancho en píxeles, puedes usar:
+    // val screenWidthPixels = screenWidth.toPx().toInt()
+
+    return screenWidth.dp
+}
+@Composable
+fun GetScreenHeight(): Dp {
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp
+
+    // Si deseas obtener el ancho en píxeles, puedes usar:
+    // val screenWidthPixels = screenWidth.toPx().toInt()
+
+    return screenHeight.dp
+}
