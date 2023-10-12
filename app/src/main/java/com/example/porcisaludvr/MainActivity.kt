32b7a2e7bf301a813573ebe6f8a1886e4b6b9507
AldairBryan <asalcedoch@unsa.edu.pe>
2023-10-12 00:51:56 -0500

    Backup

--- app/src/main/java/com/example/porcisaludvr/MainActivity.kt
+++ app/src/main/java/com/example/porcisaludvr/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -27,6 +28,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
@@ -97,6 +99,7 @@ fun MainScreen(navController: NavHostController) {
                             .width(GetScreenWidth() / 2)
                             .height(GetScreenHeight()/3)
                             .background(Color.Transparent)
+                            .clip(RoundedCornerShape(16.dp))
                             .padding(8.dp)
                     ) {
                         Column(
