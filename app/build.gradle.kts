5a1bf114ea94367bb5ef4ac9a743c106c23d2ef1
AldairBryan <asalcedoch@unsa.edu.pe>
2023-10-15 21:05:48 -0500

    Esceneas de Enfermedades

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,5 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     implementation ("androidx.navigation:navigation-compose:2.5.3")
+    implementation ("io.github.sceneview:arsceneview:0.10.2")
 }
