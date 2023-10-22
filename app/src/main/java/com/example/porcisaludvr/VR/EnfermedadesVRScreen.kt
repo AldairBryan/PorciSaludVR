fbd37f137efbc1d4a527b3ae71438236168f471b
AldairBryan <asalcedoch@unsa.edu.pe>
2023-10-21 19:24:19 -0500

    Se añadio titulos en VR

--- app/src/main/java/com/example/porcisaludvr/VR/EnfermedadesVRScreen.kt
+++ app/src/main/java/com/example/porcisaludvr/VR/EnfermedadesVRScreen.kt
@@ -2,14 +2,19 @@ package com.example.porcisaludvr.VR
 
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,12 +29,18 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.porcisaludvr.R
+import com.example.porcisaludvr.ui.theme.Itim
 import com.example.porcisaludvr.ui.theme.Translucent
 import com.google.ar.core.Config
 import io.github.sceneview.ar.ARScene
@@ -65,7 +76,6 @@ fun EnfermedadesVRScreen(navController: NavHostController, enfermedad: String) {
 @Composable
 fun Enfermedades(startIndex: Int, modifier: Modifier, onClick:(String)->Unit) {
 
-
     val itemsList = listOf(
         Enfermedad("ppc", R.drawable.sel_vr_ppc),
         Enfermedad("sarna", R.drawable.sel_vr_sarna),
@@ -96,7 +106,6 @@ fun Enfermedades(startIndex: Int, modifier: Modifier, onClick:(String)->Unit) {
             Icon(painter = painterResource(id = R.drawable.baseline_arrow_forward_ios_24), contentDescription ="next")
         }
     }
-
 }
 
 @Composable
@@ -170,6 +179,61 @@ fun ARScreen(model:String) {
         )
         Log.e("errorloading","ERROR LOADING MODEL")
     }
+    showTitle(num = model)
+}
 
+@Composable
+fun TextOverlay(text: String, modifier: Modifier = Modifier) {
+    Text(
+        text = text,
+        overflow = TextOverflow.Ellipsis,
+        fontSize = 19.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = Itim,
+        color = Color.White,
+        modifier = modifier
+            .background(Color.Black)
+            .padding(16.dp)
+    )
+}
+@Composable
+fun showTitle (num: String){
+    var textTitle: String = ""
+    var colorText: Color = Color(0,142,255,141)
+
+    when (num){
+        "ppc" -> { textTitle = "Peste Porcina"
+        colorText= Color(0, 142, 141, 255)}
+        "sarna" -> { textTitle = "Sarna Sarcoptica"
+        colorText = Color(137, 73, 136, 255)}
+        "neumonia" -> { textTitle = "Neumonía Enzoótica"
+        colorText = Color(211, 58, 84, 255)}
+    }
+    showTextTitleHelper(colorGeneral = colorText, titulo = textTitle)
+}
+@Composable
+fun showTextTitleHelper(colorGeneral: Color, titulo: String){
+    Box(
+        modifier = Modifier
+            .padding(24.dp)
+            .shadow(elevation = 4.dp, shape = RoundedCornerShape(24.dp))
+            .border(
+                width = 5.dp, // Ancho del borde
+                color = colorGeneral, // Color del borde
+                shape = RoundedCornerShape(24.dp) // Bordes redondeados
+            )
+            .background(Color.Transparent),
+        contentAlignment = Alignment.CenterStart
+    ) {
+            Text(
+                text = titulo,
+                fontFamily = Itim,
+                fontSize = 22.sp,
+                color = colorGeneral,
+                textAlign = TextAlign.Left,
+                modifier = Modifier.padding(10.dp,5.dp)
+            )
+            Spacer(modifier = Modifier.width(13.dp))
+    }
 }
 data class  Enfermedad(var name:String, var imageID:Int)
