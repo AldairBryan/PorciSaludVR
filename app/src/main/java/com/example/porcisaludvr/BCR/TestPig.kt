ad35822774c078e92afc4a8a2704021df7ef2d38
AldairBryan <asalcedoch@unsa.edu.pe>
2023-11-01 19:06:36 -0500

    Añadio preguntas

--- app/src/main/java/com/example/porcisaludvr/BCR/TestPig.kt
+++ app/src/main/java/com/example/porcisaludvr/BCR/TestPig.kt
@@ -32,14 +32,34 @@ fun TestPig (navController: NavHostController){
     }
     var currentQuestionIndex by remember { mutableStateOf(0) }
     val questions = listOf(
-        Question("Pregunta 1", listOf("Opción A", "Opción B", "Opción C")),
-        Question("Pregunta 2", listOf("Opción A", "Opción B", "Opción C")),
-        Question("Pregunta 3", listOf("Opción A", "Opción B", "Opción C")),
-        // Agrega más preguntas aquí
+        Question("1. ¿El cerdo presenta signos de Fiebre?", listOf("Si","No")),
+        Question("2. ¿El cerdo padece de Diarrea?", listOf("Si","No")),
+        Question("3. ¿El cerdo presenta anorexia?", listOf("Si","No")),
+        Question("4. ¿El cerdo experimenta estreñimiento?", listOf("Si","No")),
+        Question("5. ¿El cerdo presenta vómitos?", listOf("Si","No")),
+        Question("6. ¿El cerdo tiene dificultad para respirar (disnea)?", listOf("Si","No")),
+        Question("7. ¿El cerdo presenta problemas de reproducción, como abortos, partos prematuros o malformaciones en las crías?", listOf("Si","No")),
+        Question("8. ¿El cerdo muestra comezón o picor en la piel?", listOf("Si","No")),
+        Question("9. ¿La piel del cerdo está roja y caliente al tacto?", listOf("Si","No")),
+        Question("10. ¿La piel del cerdo se encuentra seca, áspera y agrietada?", listOf("Si","No")),
+        Question("11. ¿El cerdo tiene heridas profundas en la piel?", listOf("Si","No")),
+        Question("12. ¿El cerdo se frota contra superficies de manera inusual? ¿El cerdo se rasca frecuentemente en las orejas, cuello y cabeza?", listOf("Si","No")),
+        Question("13. ¿El cerdo está experimentando estrés?", listOf("Si","No")),
+        Question("14. ¿El cerdo presenta alopecia (ausencia de pelo) en las orejas y el cuello?", listOf("Si","No")),
+        Question("15. ¿El cerdo tiene lesiones en la nariz?", listOf("Si","No")),
+        Question("16. ¿El cerdo muestra signos de depresión?", listOf("Si","No")),
+        Question("17. ¿El cerdo presenta escalofríos?", listOf("Si","No")),
+        Question("18. ¿El cerdo camina de manera rígida?", listOf("Si","No")),
+        Question("19. ¿El cerdo tiene manchas rojas en la piel?, como en el abdomen, las orejas y la parte interna", listOf("Si","No")),
+        Question("20. ¿El cerdo sufre de hemorragias en la piel?", listOf("Si","No")),
+        Question("21. ¿El cerdo tiene conjuntivitis con ojos rojos? (Enrojecimiento y/o secrecion en los ojos)", listOf("Si","No")),
+        Question("22. ¿El cerdo ha tenido convulsiones?", listOf("Si","No")),
+        Question("23. ¿El cerdo muestra ataxia, es decir, falta de coordinación y trastornos nerviosos?", listOf("Si","No")),
+        Question("24. ¿El cerdo tose de manera anormal?", listOf("Si","No")),
     )
 
     fun onNextClicked(selectedAnswer: String) {
-        respuesta += selectedAnswer
+        respuesta += selectedAnswer+":"
         currentQuestionIndex++
     }
 
@@ -71,11 +91,6 @@ fun QuestionItem(question: Question, onNextClicked: (String) -> Unit) {
     var selectedAnswer by remember {
         mutableStateOf("")
     }
-    var customAnswer by remember {
-        mutableStateOf("")
-    }
-    var isAnswerSelected by remember { mutableStateOf(false)
-    }
 
     Column(
         modifier = Modifier
@@ -96,7 +111,6 @@ fun QuestionItem(question: Question, onNextClicked: (String) -> Unit) {
                     selected = option == selectedAnswer,
                     onClick = {
                         selectedAnswer = option
-                        isAnswerSelected = true
                     }
                 )
                 Spacer(modifier = Modifier.width(8.dp))
@@ -106,46 +120,10 @@ fun QuestionItem(question: Question, onNextClicked: (String) -> Unit) {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            RadioButton(
-                selected = "Respuesta personalizada" == selectedAnswer,
-                onClick = {
-                    selectedAnswer = "Respuesta personalizada"
-                    customAnswer=""
-                }
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(text = "Otra respuesta")
-
-            Spacer(modifier = Modifier.width(16.dp))
-
-            if ("Respuesta personalizada" == selectedAnswer) {
-                    OutlinedTextField(
-                    value = customAnswer,
-                    onValueChange = {
-                        customAnswer = it
-                        isAnswerSelected = it.isNotBlank()
-                    },
-                    label = { Text("Ingresa tu respuesta") },
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(4.dp)
-                )
-            }
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-
         Button(
             onClick = {
-                if (isAnswerSelected) {
-                    if (selectedAnswer == "Respuesta personalizada"){
-                        onNextClicked(customAnswer)
-                    }else {
-                        onNextClicked(selectedAnswer)
-                    }
+                if (selectedAnswer.isNotBlank()) {
+                    onNextClicked(selectedAnswer)
                     selectedAnswer = ""
                 }
             },
