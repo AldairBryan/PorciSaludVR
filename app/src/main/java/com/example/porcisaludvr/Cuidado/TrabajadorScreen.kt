4c36b509bc3177cef75e1dd4a9ac3b863c234815
AldairBryan <asalcedoch@unsa.edu.pe>
2023-11-25 15:32:44 -0500

    Se termino TrabajadorScreen

--- app/src/main/java/com/example/porcisaludvr/Cuidado/TrabajadorScreen.kt
+++ app/src/main/java/com/example/porcisaludvr/Cuidado/TrabajadorScreen.kt
@@ -28,7 +28,7 @@ import com.example.porcisaludvr.ui.theme.Itim
 
 @Composable
 fun TrabajadorScreen(navHostController: NavHostController){
-    var colorInfo: Color = Color(0,142,141,255)
+    var colorInfo: Color = Color(211,58,84,255)
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -37,7 +37,7 @@ fun TrabajadorScreen(navHostController: NavHostController){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text ="Peste Porcina Clasica",
+        Text(text ="Salud y Bienestar del Trabajador",
             fontFamily = Itim,
             fontSize = 35.sp,
             fontWeight = FontWeight.Bold,
@@ -47,7 +47,7 @@ fun TrabajadorScreen(navHostController: NavHostController){
         )
         // Imagen
         Image(
-            painter = painterResource(id = R.drawable.m2_info_1_ppc),
+            painter = painterResource(id = R.drawable.m3_cuidado_0_bienestar_8),
             contentDescription = null,
             modifier = Modifier
                 .size(200.dp)
@@ -56,44 +56,16 @@ fun TrabajadorScreen(navHostController: NavHostController){
 
         // Informacion
         ExpandableCard(title = "Informacion",
-            description = " Es una ",
+            description = "Constituye el conjunto de elementos interrelacionados que tienen como objetivo la seguridad y salud en el centro de trabajo en la que se promueve la cultura de prevención de riesgos. La agro industria por su naturaleza, a pesar que no aparece como una actividad de riesgo en la legislación vigente, conlleva potenciales riesgos vinculados a la salud y seguridad ocupacional.",
             colorTitle = colorInfo)
         Spacer(modifier = Modifier.height(6.dp))
 
         //Sintomas
-        ExpandableCard(title = "Sintomas",
-            description = "En la",
+        ExpandableCard(title = "Investigaciones",
+            description = "De acuerdo a la investigación realizada por el instituto Salud y trabajo (ISAT 2005), con frecuencia los trabajadores de este sector están expuestos a factores físicos, químicos, biológicos, psicosociales y ergonómicos. Los síntomas más habituales son las enfermedades bronco-pulmonares, la intoxicación por contacto con agro químicos (fertilizantes- pesticidas), la radiación solar y el riesgo ergonómico por desconocimiento y ausencia de políticas preventivas de salud y seguridad en el trabajo que diferencien las necesidades de salud y protección a hombres y mujeres.",
             colorBG = colorInfo,
             colorTitle = Color.White,
             colorText = Color.White)
-        Spacer(modifier = Modifier.height(6.dp))
-
-        //Lesiones de Necropsia
-        ExpandableCard(title = "Lesiones de Necropsia",
-            description = "En l ",
-            colorTitle = colorInfo)
-        Spacer(modifier = Modifier.height(6.dp))
-
-        //Tratamiento
-        ExpandableCard(title = "Tratamiento",
-            description = "Para el ",
-            colorTitle = Color.White,
-            colorText = Color.White,
-            colorBG = colorInfo)
-        Spacer(modifier = Modifier.height(6.dp))
-
-        //Prevencion y control
-        ExpandableCard(title = "Prevencion y Control",
-            description = "La medida más im",
-            colorTitle = colorInfo)
-        Spacer(modifier = Modifier.height(6.dp))
-
-        //PRINCIPALES MEDIDAS Que TOMAR ANTE UN BROTE
-        ExpandableCard(title = "Principales medidas que tomar ante un brote",
-            description = "Vacunar o",
-            colorTitle = Color.White,
-            colorText = Color.White,
-            colorBG = colorInfo)
         Spacer(modifier = Modifier.height(6.dp))
     }
 }
